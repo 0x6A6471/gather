e8cae86fc6b628e9ff6a35d973387324961af9bb
Jake Quinter <hello@jakequinter.io>
2024-03-06 17:48:49 -0500

    sep of concerns

--- server/bin/main.ml
+++ server/bin/main.ml
@@ -1,9 +1,5 @@
 open Lwt.Syntax
-open Ppx_yojson_conv_lib.Yojson_conv.Primitives
-
-module type DB = Caqti_lwt.CONNECTION
-
-module T = Caqti_type
+(* module T = Caqti_type *)
 
 let init_pool () =
   let uri = Uri.make ~scheme:"sqlite3" ~path:"./registry.db" () in
@@ -13,44 +9,6 @@ let init_pool () =
 ;;
 
 let pool = init_pool ()
-
-type t =
-  { id : int
-  ; email : string
-  }
-[@@deriving yojson]
-
-type user =
-  { email : string
-  ; password : string
-  }
-[@@deriving yojson]
-
-let find_user_by_id id pool =
-  let query =
-    [%rapper
-      get_one
-        {sql| SELECT @int{id}, @string{email} FROM users WHERE id = %int{id} |sql}
-        record_out]
-  in
-  let* result = Caqti_lwt.Pool.use (fun db -> query db ~id) pool in
-  match result with
-  | Ok user -> Lwt.return (Some user)
-  | Error _ -> Lwt.return None
-;;
-
-let validate_user_credentials email password pool =
-  let query =
-    [%rapper
-      get_one
-        {sql| SELECT @int{id}, @string{email} FROM users WHERE email = %string{email} AND password = %string{password} |sql}
-        record_out]
-  in
-  let* result = Caqti_lwt.Pool.use (fun db -> query db ~email ~password) pool in
-  match result with
-  | Ok user -> Lwt.return (Some user)
-  | Error _ -> Lwt.return None
-;;
 
 let cors_middleware inner_handler req =
   let new_headers =
@@ -77,54 +35,17 @@ let cors_middleware inner_handler req =
 ;;
 
 let () =
-  Dream.run
+  Dream.run ~interface:"0.0.0.0"
   @@ Dream.logger
   @@ Dream.sql_pool "sqlite3:./registry.db"
   (* @@ Dream.origin_referrer_check *)
   @@ Dream.sql_sessions ~lifetime:3600.0
   @@ cors_middleware
   @@ Dream.router
-       [ Dream.get "/" (fun _request ->
-           let json_string = {|{ "status": "ok" }|} in
-           let json = Yojson.Safe.from_string json_string in
-           json |> Yojson.Safe.to_string |> Dream.json)
-       ; Dream.get "/me" (fun request ->
-           let session = Dream.session "user_id" request in
-           match session with
-           | Some user_id ->
-             let user_id = int_of_string user_id in
-             let* user_opt = find_user_by_id user_id pool in
-             begin
-               match user_opt with
-               | Some user ->
-                 let user_json = yojson_of_t user in
-                 Dream.json (Yojson.Safe.to_string user_json)
-               | None ->
-                 let* _ = Dream.invalidate_session request in
-                 let err_json = {|{ "error": "user not found" }|} in
-                 Dream.json ~status:`Unauthorized err_json
-             end
-           | None ->
-             let* _ = Dream.invalidate_session request in
-             Dream.json
-               ~status:`Unauthorized
-               "{ \"error\": \"No active session.\" }")
-       ; Dream.post "/login" (fun request ->
-           let* body = Dream.body request in
-           let user = body |> Yojson.Safe.from_string |> user_of_yojson in
-           let* user_opt =
-             validate_user_credentials user.email user.password pool
-           in
-           match user_opt with
-           | Some user ->
-             let* _ =
-               Dream.put_session "user_id" (string_of_int user.id) request
-             in
-             let user_json = yojson_of_t user in
-             Dream.json (Yojson.Safe.to_string user_json)
-           | None ->
-             let* _ = Dream.invalidate_session request in
-             let err_json = {|{ "error": "invalid credentials" }|} in
-             Dream.json ~status:`Unauthorized err_json)
-       ]
+       ([ Dream.get "/" (fun _request ->
+            let json_string = {|{ "status": "ok" }|} in
+            let json = Yojson.Safe.from_string json_string in
+            json |> Yojson.Safe.to_string |> Dream.json)
+        ]
+        @ Routes.Auth.routes pool)
 ;;
