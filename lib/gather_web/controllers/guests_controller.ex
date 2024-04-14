a594b57cd41281fa2b4fee333216668a9f9ca89c
Jake Quinter <hello@jakequinter.io>
2024-04-13 20:14:22 -0400

    remove logging

--- lib/gather_web/controllers/guests_controller.ex
+++ lib/gather_web/controllers/guests_controller.ex
@@ -11,27 +11,24 @@ defmodule GatherWeb.GuestsController do
   end
 
   defp create(conn, %{"guest" => guest_params, "user_id" => user_id}, _info) do
-    IO.inspect(user_id,
-      label: "user_id"
-    )
-
-    IO.inspect(guest_params,
-      label: "guest_params"
-    )
-
     user = Accounts.get_user!(user_id)
 
-    case Guests.create_guest(user, guest_params) do
-      {:ok, guest} ->
-        dbg(guest)
+    if user do
+      case Guests.create_guest(user, guest_params) do
+        {:ok, guest} ->
+          dbg(guest)
 
-        conn
-        |> put_flash(:info, "#{guest.name} added successfully.")
-        |> redirect(to: "/guests")
+          conn
+          |> put_flash(:info, "#{guest.name} added successfully.")
+          |> redirect(to: "/guests")
 
-      {:error, _changeset} ->
-        conn
-        |> put_flash(:error, "Failed to create guest.")
+        {:error, _changeset} ->
+          conn
+          |> put_flash(:error, "Failed to create guest.")
+      end
+    else
+      conn
+      |> put_flash(:error, "User not found.")
     end
   end
 end
