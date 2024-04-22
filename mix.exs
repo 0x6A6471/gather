2f57ec4a0115dd64335f655c7bb30d27413dfe78
Jake Quinter <hello@jakequinter.io>
2024-04-22 17:58:22 -0400

    action tests

--- mix.exs
+++ mix.exs
@@ -68,6 +68,7 @@ defmodule Gather.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      "test.ci": ["ecto.drop", "ecto.create --quiet", "ecto.migrate", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind default", "esbuild default"],
       "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
